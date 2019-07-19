b449be734ef04537572adba3b348cae491b411ba
zhangkun19 <zhangkun19@jd.com>
2019-07-19 09:48:38 +0800

    jointhread

--- src/test/java/thread/appcontent/AppContextMain.java
+++ src/test/java/thread/appcontent/AppContextMain.java
@@ -1,5 +1,8 @@
 package thread.appcontent;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * PROJECT_name: github
  * package: thread
@@ -11,19 +14,18 @@ package thread.appcontent;
 public class AppContextMain {
 
     public static void main(String[] args) {
-        String[] names = { "骆昊", "王大锤", "张三丰", "杨过", "李莫愁" };   // 5位哲学家的名字
-//      ExecutorService es = Executors.newFixedThreadPool(AppContext.NUM_OF_PHILO);
+        String[] names = {"骆昊", "王大锤", "张三丰", "杨过", "李莫愁"};   // 5位哲学家的名字
+        ExecutorService es = Executors.newFixedThreadPool(AppContext.NUM_OF_PHILO);
 // 创建固定大小的线程池
-//      for(int i = 0, len = names.length; i < len; ++i) {
-        //          es.execute(new Philosopher(i, names[i]));
+        for (int i = 0, len = names.length; i < len; ++i) {
+            es.execute(new Philosopher(i, names[i]));
 // 启动线程
-
-//      }
-
-//      es.shutdown();
-        for(int i = 0, len = names.length; i < len; ++i) {
-            new Thread(new Philosopher(i, names[i])).start();
         }
+
+        es.shutdown();
+//        for(int i = 0, len = names.length; i < len; ++i) {
+//            new Thread(new Philosopher(i, names[i])).start();
+//        }
     }
 
 }
