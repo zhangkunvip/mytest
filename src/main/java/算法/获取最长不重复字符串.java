9ab655b43af08c79f68841775874d85138e0a123
zhangkun19 <zhangkun19@jd.com>
2019-09-18 16:46:39 +0800

    ~

---
+++ src/main/java/算法/获取最长不重复字符串.java
@@ -0,0 +1,76 @@
+package 算法;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * PROJECT_name: mytest
+ * package: 算法
+ * creat_user: zhangkun19
+ * creat_date: 2019/09/05
+ * creat_time: 10:05
+ * describe: TODO
+ **/
+public class 获取最长不重复字符串 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("qwefwpo98"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            // try to extend the range [i, j]
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstring1(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstringa(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (allUnique(s, i, j)){
+                    ans = Math.max(ans, j - i);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static boolean  allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) {
+                return false;
+            }
+            set.add(ch);
+        }
+        return true;
+    }
+}
---
+++ src/test/java/junit/ExceptionTest.java
@@ -0,0 +1,35 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author shuang.kou
+ */
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ExceptionTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void should_return_400_if_param_not_valid() throws Exception {
+        mockMvc.perform(get("/api/illegalArgumentException"))
+                .andExpect(status().is(400))
+                .andExpect(jsonPath("$.message").value("参数错误!"));
+    }
+
+    @Test
+    void should_return_404_if_resourse_not_found() throws Exception {
+        mockMvc.perform(get("/api/resourceNotFoundException"))
+                .andExpect(status().is(404))
+                .andExpect(jsonPath("$.message").value("Sorry, the resourse not found!"));
+    }
+}
