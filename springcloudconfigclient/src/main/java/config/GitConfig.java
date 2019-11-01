27b56e9ff855fee3857bd9ea4c86ea0d0b17e835
zhangkun19 <zhangkun19@jd.com>
2019-11-01 10:23:47 +0800

    springtest!

---
+++ springcloudconfigclient/src/main/java/config/GitConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class GitConfig {
+
+    @Value("${data.env}")
+    private String env;
+
+    @Value("${data.user.username}")
+    private String username;
+
+    @Value("${data.user.password}")
+    private String password;
+
+}
